a9985174ec5fac8cfb779df79f4dd8c2c5cdb682
ilyass-rakas <ilyas.rakas@uit.ac.ma>
2026-03-12 17:00:20 +0000

    raskon yarbi ikhdem

--- raskon-appart/vite.config.js
+++ raskon-appart/vite.config.js
@@ -1,44 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    // Optimize bundle size
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
-    // Asset inline limit
-    assetsInlineLimit: 4096,
-    // Rollup options
-    rollupOptions: {
-      output: {
-        // Optimize chunk naming for better caching
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.')[1];
-          if (/png|jpe?g|gif|svg|webp/.test(extType)) {
-            return `images/[name]-[hash][extname]`;
-          } else if (/woff|woff2|eot|ttf|otf/.test(extType)) {
-            return `fonts/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'js/[name]-[hash].js',
-        entryFileNames: 'js/[name]-[hash].js',
-      }
-    }
-  },
-  // Dev server config
-  server: {
-    headers: {
-      'X-Content-Type-Options': 'nosniff',
-      'X-Frame-Options': 'SAMEORIGIN',
-      'X-XSS-Protection': '1; mode=block',
-      'Referrer-Policy': 'strict-origin-when-cross-origin'
-    }
-  }
+  plugins: [react()]
 })
